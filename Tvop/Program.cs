eee0354275617619cf0151540f17c5d2aa5c686f
Vlados <vlad13051999@gmail.com>
2019-04-08 22:49:29 +0300

    Add project files.

---
+++ Tvop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Tvop
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = GetLength(1);
+            var b = GetLength(2);
+            var c = GetLength(3);
+            if (a + b >= c && a + c >= b && b + c >= a)
+            {
+                if (a != c && b != c && a != b)
+                {
+                    Console.WriteLine("Треугольник - разносторонний");
+                }
+                else if (a == b && a != c || c == b && a != c || a == c && b != c)
+                {
+                    Console.WriteLine("Треуголник - равнобедренный");
+                }
+                else
+                {
+                    Console.WriteLine("Треуголник - равносторонний");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Невозможно создать треугольник");
+            }
+        }
+
+        private static int GetLength(int partNumber)
+        {
+            int result;
+            while (true)
+            {
+                Console.Write("Введите " + partNumber + " сторону: ");
+                if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Неверные входные данные");
+            }
+
+            return result;
+        }
+    }
+}
